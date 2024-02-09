c785da6dc48a8430c04cbf105e03968be01046b6
Adrian <andrian.melnic2003@gmail.com>
2024-02-09 14:39:48 +0200

    Laborator2

---
+++ Laborator2/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Nume: Melnic</li>
+        <li>Prenume: Adrian</li>
+        <li>Grupa: CR-221</li>
+        <li>An : 2</li>
+      </ul>
+  </div>
+  );
+}
+
+export default App;
